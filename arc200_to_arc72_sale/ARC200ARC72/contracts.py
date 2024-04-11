0173c7a3592134985adbf3f92d4faa98d66ce7d0
askole1n <78314974+askole1n@users.noreply.github.com>
2024-04-11 14:56:19 +0200

    Add files via upload
    Operation delete (cancel) for before/during/After sale

--- arc200_to_arc72_sale/ARC200ARC72/contracts.py
+++ arc200_to_arc72_sale/ARC200ARC72/contracts.py
@@ -2,78 +2,279 @@ from pyteal import *
 
 def approval_program():
     # PARAMETERS
-    owner_key = Bytes("owner")  # Owner of the NFT
-    nft_id_key = Bytes("nft_id")  # Unique identifier for the NFT
-    
-    # Utility function to check if the sender is the owner of the NFT
-    @Subroutine(TealType.uint64)
-    def is_owner(sender: Expr) -> Expr:
-        return sender == App.globalGet(owner_key)
-    
-    # ARC-72 methods
-    # arc72_ownerOf implementation
-    @Subroutine(TealType.bytes)
-    def ownerOf(token_id: Expr) -> Expr:
-        Assert(token_id == App.globalGet(nft_id_key)),
-        return App.globalGet(owner_key)
-    
-    # arc72_transferFrom implementation  
+    seller_key = Bytes("seller")
+    nft_id_key = Bytes("nft_id")
+    nft_app_id_key = Bytes("nft_app_id")
+    nft_price = Bytes("price")
+    start_time_key = Bytes("start")
+    arc200_app_id_key =  Bytes("arc200_app_id") # New parameter for ARC-200 asset ID
+
     @Subroutine(TealType.none)
-    def transferFrom(from_addr: Expr, to_addr: Expr, token_id: Expr) -> Expr:
+    def transferNFT(to_account: Expr) -> Expr:
+        return Seq(
+                InnerTxnBuilder.Begin(),
+                InnerTxnBuilder.SetFields({
+                    TxnField.type_enum: TxnType.ApplicationCall,
+                    TxnField.application_id: App.globalGet(nft_app_id_key), 
+                    TxnField.accounts: [to_account, Global.current_application_address()],
+                    TxnField.on_completion: OnComplete.NoOp,
+                    TxnField.applications: [App.globalGet(nft_app_id_key)],
+                    TxnField.application_args: [
+                        Bytes("arc72_transferFrom"),
+                        Global.current_application_address(),
+                        to_account,  # Assuming to_account is already a byte array
+                        Itob(App.globalGet(nft_id_key))  # Assuming this returns a byte array or is converted appropriately
+                    ],
+                }),
+                InnerTxnBuilder.Submit(),
+            )
+
+        
+    ##############################################################################################
+    @Subroutine(TealType.none)
+    def FetchApprovedARC200ToEscrow(owner: Expr, amount: Expr) -> Expr:
+        return Seq(
+            # Make sure this contract is Opted in to the ARC200 before trying to transferFrom
+            # Opt-in to the payment token (ARC200) to be able to have a sale balance (if ARC200 is implemented like that?)
+            InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields({
+                TxnField.type_enum: TxnType.ApplicationCall,
+                TxnField.application_id: App.globalGet(arc200_app_id_key),
+                TxnField.applications: [App.globalGet(arc200_app_id_key)],
+                TxnField.accounts: [Gtxn[1].accounts[0], Gtxn[1].accounts[1], Global.current_application_address()],
+                TxnField.on_completion: OnComplete.OptIn,
+            }),
+            InnerTxnBuilder.Submit(),
+            # Fetch the tokens we have been promised/approved
+
+            InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields({
+                TxnField.type_enum: TxnType.ApplicationCall,
+                TxnField.application_id: App.globalGet(arc200_app_id_key),
+                TxnField.applications: [App.globalGet(arc200_app_id_key)],
+                TxnField.accounts: [Gtxn[1].accounts[0], Gtxn[1].accounts[1], Global.current_application_address()],
+                TxnField.application_args: [
+                    Bytes("arc200_transferFrom"),
+                    owner,
+                    Global.current_application_address(),
+                    Itob(amount)
+                ],
+            }),
+            InnerTxnBuilder.Submit(),   
+        )
+
+    @Subroutine(TealType.none)
+    def SendARC200ToSeller(amount: Expr) -> Expr:
+        return Seq(
+            InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields({
+                TxnField.type_enum: TxnType.ApplicationCall,
+                TxnField.application_id: App.globalGet(arc200_app_id_key),
+                TxnField.accounts: [Gtxn[1].accounts[0], Gtxn[1].accounts[1],Global.current_application_address()],
+                TxnField.applications: [App.globalGet(arc200_app_id_key)],
+                TxnField.application_args: [
+                    Bytes("arc200_transfer"),
+                    App.globalGet(seller_key),
+                    Itob(amount)
+                ],
+                TxnField.on_completion: OnComplete.NoOp
+            }),
+            InnerTxnBuilder.Submit(),
+        )
+        
+    ################################################################################################################################################
+    @Subroutine(TealType.none)
+    def closeAccountTo(account: Expr) -> Expr:
+        return If(Balance(Global.current_application_address()) != Int(0)).Then(
+            Seq(
+                InnerTxnBuilder.Begin(),
+                InnerTxnBuilder.SetFields(
+                    {
+                        TxnField.type_enum: TxnType.Payment,
+                        TxnField.close_remainder_to: account,
+                    }
+                ),
+                InnerTxnBuilder.Submit(),
+            )
+        )
+
+    @Subroutine(TealType.uint64)
+    def allowance_check(owner: Expr) -> Expr:
+        return Seq(
+            InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields({
+                TxnField.type_enum: TxnType.ApplicationCall,
+                TxnField.application_id: App.globalGet(arc200_app_id_key),
+                TxnField.applications: [App.globalGet(arc200_app_id_key)],
+                TxnField.accounts: [Gtxn[1].accounts[0], Gtxn[1].accounts[1], Global.current_application_address()],
+                TxnField.application_args: [
+                    Bytes("arc200_allowance"),              # // method
+                    owner,                                  # // allower
+                    Global.current_application_address()    # // spendee
+                ],
+            }),
+            InnerTxnBuilder.Submit(),
+            Return(InnerTxn.last_valid())
+        )
+
+    @Subroutine(TealType.bytes)
+    def arc72_owner(tokenId: Expr) -> Expr:
+        return Seq(
+            InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields({
+                TxnField.type_enum: TxnType.ApplicationCall,
+                TxnField.application_id: App.globalGet(nft_app_id_key),
+                TxnField.applications: [App.globalGet(arc200_app_id_key), App.globalGet(nft_app_id_key)],
+                TxnField.accounts: [Global.current_application_address()],
+                TxnField.application_args: [
+                    Bytes("arc72_ownerOf"),              # // method
+                    tokenId                              # // tokenId
+                ],
+            }),
+            InnerTxnBuilder.Submit(),
+            Return(InnerTxn.last_log())
+        )
+
+    ################################################################################################################################################
+    @Subroutine(TealType.none)
+    def clear_state_from_app() -> Expr:
         return Seq([
-            Assert(token_id == App.globalGet(nft_id_key)),
-            Assert(from_addr == App.globalGet(owner_key)),
-            App.globalPut(owner_key, to_addr),
-            Log(Concat(Bytes("arc72_Transfer("), 
-                       from_addr, Bytes(","), 
-                       to_addr, Bytes(","), 
-                       Itob(token_id), Bytes(")"))),
+            InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields({
+                TxnField.type_enum: TxnType.ApplicationCall,
+                TxnField.application_id: App.globalGet(arc200_app_id_key),
+                TxnField.applications: [App.globalGet(arc200_app_id_key), App.globalGet(nft_app_id_key)],
+                TxnField.accounts: [Global.current_application_address()],
+                TxnField.on_completion: OnComplete.ClearState,  # Opt-out of the ARC200 contract
+            }),
+            InnerTxnBuilder.Submit(),
         ])
-    
-    # On creation, set the creator as the owner and store the NFT ID
-    on_creation = Seq([
-        App.globalPut(owner_key, Txn.sender()),
-        App.globalPut(nft_id_key, Btoi(Txn.application_args[0])),
+        
+
+    ################################################################################################################################################
+    # VARIABLE SETUP
+    note_content = Bytes("This is a note field")
+
+    on_create_start_time = Btoi(Txn.application_args[3])
+    on_create = Seq(
+        App.globalPut(seller_key, Txn.application_args[0]),
+        App.globalPut(nft_app_id_key, Btoi(Txn.application_args[1])),
+        App.globalPut(nft_id_key, Btoi(Txn.application_args[2])),
+        App.globalPut(start_time_key, on_create_start_time),
+        App.globalPut(nft_price, Btoi(Txn.application_args[4])),
+        App.globalPut(arc200_app_id_key, Btoi(Txn.application_args[5])),
+        Approve(),
+    )
+
+    ##############################################################################################################################
+    on_buy = Seq(
+        Assert(
+            allowance_check(Txn.sender()) >= App.globalGet(nft_price)
+        ),
+        Seq(
+            # Fetch nft_price arc200 tokens on the escrow sale contract from buyer
+            FetchApprovedARC200ToEscrow(Txn.sender(), App.globalGet(nft_price)),
+            # transfer these back to the seller
+            SendARC200ToSeller(App.globalGet(nft_price)),
+            # transfer NFT to buyer
+            transferNFT(Txn.sender()),
+            Approve()
+        ),
+        Reject(),
+    )
+
+    ################################################################################################################################################
+    new_price = Btoi(Txn.application_args[1])
+    on_update_price = Seq(
+        Assert(
+            Or(
+                # sender must either be the seller or the Sale creator
+                Txn.sender() == App.globalGet(seller_key),
+                Txn.sender() == Global.creator_address()
+            )
+        ),
+        Assert( new_price > Int(0) ),
+        ## UPDATE NEW PRICE
+        App.globalPut(nft_price, new_price),
         Approve()
-    ])
+    )
+
+    #######################
+    ################################################################################################################################################
+    #######################
     
     on_call_method = Txn.application_args[0]
     on_call = Cond(
-        [on_call_method == Bytes("arc72_transferFrom"), 
-            Seq(        
-                transferFrom(
-                    Txn.application_args[1], 
-                    Txn.application_args[2],
-                    Btoi(Txn.application_args[3])),
-            Approve()
-            )
-        ],
-        [on_call_method == Bytes("arc72_ownerOf"), 
-            Seq(    
-                # Emit the owner address instead of returning it
-                Log(ownerOf(Btoi(Txn.application_args[1]))),
-                Approve()
-            )
-        ],
+        [on_call_method == Bytes("buy"), on_buy],
+        [on_call_method == Bytes("update_price"), on_update_price]
     )
+
     
-    # Program start
+    # Logic to clear specific local state entries related to the ARC200 app
+    clear_logic = Seq([
+        # Example: Delete local state entries related to ARC200 interactions
+        # Adjust the key names based on your actual use case
+        App.localDel(Txn.sender(), Bytes("balance")),
+        Return(Int(1))
+    ])
+
+    on_delete = Seq(
+        Assert(
+            Or(
+                # sender must either be the seller or the Sale creator
+                Txn.sender() == App.globalGet(seller_key),
+                Txn.sender() == Global.creator_address()
+            )
+        ),
+        # Check if current app is opted into ARC200 app
+        If(App.optedIn(Global.current_application_address(), App.globalGet(arc200_app_id_key))).Then(
+            clear_state_from_app()
+        ),
+        If(
+            arc72_owner(Itob(App.globalGet(nft_id_key))) ==  Global.current_application_address()
+        ).
+        Then(
+            transferNFT(App.globalGet(seller_key))
+        ),
+        # send remaining funds to the seller
+        closeAccountTo(App.globalGet(seller_key)),
+        Approve(),
+    )
+
+
+    #######################
+    ################################################################################################################################################
+    #######################
+
     program = Cond(
-        [Txn.application_id() == Int(0), on_creation],
+        [Txn.application_id() == Int(0), on_create],
         [Txn.on_completion() == OnComplete.NoOp, on_call],
-        [Txn.on_completion() == OnComplete.CloseOut, Approve()]
+        [
+            Txn.on_completion() == OnComplete.DeleteApplication,
+            on_delete,
+        ],
+        [
+            Or(
+                Txn.on_completion() == OnComplete.OptIn,
+                Txn.on_completion() == OnComplete.CloseOut,
+                Txn.on_completion() == OnComplete.UpdateApplication,
+            ),
+            Reject(),
+        ],
     )
-    
+
     return program
+
 
 def clear_state_program():
     return Approve()
 
+
 if __name__ == "__main__":
-    with open("arc72_approval.teal", "w") as f:
-        compiled = compileTeal(approval_program(), mode=Mode.Application, version=5)
+    with open("approval.teal", "w") as f:
+        compiled = compileTeal(approval_program(), mode=Mode.Application, version=9)
         f.write(compiled)
 
-    with open("arc72_clear_state.teal", "w") as f:
-        compiled = compileTeal(clear_state_program(), mode=Mode.Application, version=5)
+    with open("clear_state.teal", "w") as f:
+        compiled = compileTeal(clear_state_program(), mode=Mode.Application, version=9)
         f.write(compiled)
--- arc200_to_arc72_sale/ARC200ARC72/operations.py
+++ arc200_to_arc72_sale/ARC200ARC72/operations.py
@@ -18,12 +18,9 @@ from .util import (
 APPROVAL_PROGRAM = b""
 CLEAR_STATE_PROGRAM = b""
 
-################
-################################################################################
-################
 
 def getContracts(client: AlgodClient) -> Tuple[bytes, bytes]:
-    """Get the compiled TEAL contracts for the Offer.
+    """Get the compiled TEAL contracts for the Sale.
 
     Args:
         client: An algod client that has the ability to compile TEAL programs.
@@ -42,47 +39,46 @@ def getContracts(client: AlgodClient) -> Tuple[bytes, bytes]:
     return APPROVAL_PROGRAM, CLEAR_STATE_PROGRAM
 
 
-################
-################################################################################
-################
-
-def create_ARC72_NFT(
+def create_ARC200_ARC72Sale_App(
     client: AlgodClient,
     sender: Account,
-    nftID: int
+    seller: str,
+    nftAppID: int,
+    nftID: int,
+    startTime: int,
+    price: int,
+    arc200_app_id: int,
 ) -> int:
-    """Create a new Offer.
+    """Create a new Sale.
 
     Args:
         client: An algod client.
-        sender: The account that will create the Offer application.
+        sender: The account that will create the Sale application.
         seller: The address of the seller that currently holds the NFT being
             auctioned.
-        nftIDs: The IDs of the NFTs being auctioned.
-        endTime: A UNIX timestamp representing the end time of the Offer. This
-            must be greater than startTime.
-        reserve: The reserve amount of the Offer. If the Offer ends without
-            a bid that is equal to or greater than this amount, the Offer will
-            fail, meaning the bid amount will be refunded to the lead bidder and
-            the NFT will return to the seller.
-        minBidIncrement: The minimum different required between a new bid and
-            the current leading bid.
+        nftAppID: The ID of the NFT application.
+        nftID: The ID of the NFT being auctioned.
+        startTime: A UNIX timestamp representing the start time of the Sale.
+            This must be greater than the current UNIX timestamp.
+        price: The price of the NFT.
+        arc200_app_id: The ID of the ARC-200 token application.
 
     Returns:
-        The ID of the newly created Offer app.
+        The ID of the newly created Sale app.
     """
-
     approval, clear = getContracts(client)
 
-    # increase the num of variables with each N > 2
-    base_u = 8
-    base_slice = 7
-    globalSchema = transaction.StateSchema(num_uints=base_u, num_byte_slices=base_slice)
+    globalSchema = transaction.StateSchema(num_uints=8, num_byte_slices=8)
     localSchema = transaction.StateSchema(num_uints=0, num_byte_slices=0)
 
-    app_args = [nftID]
-    # app_args.append( nftID.to_bytes(8, "big") )
-
+    app_args = [
+        encoding.decode_address(seller),
+        nftAppID.to_bytes(8, "big"),
+        nftID.to_bytes(8, "big"),
+        startTime.to_bytes(8, "big"),
+        price.to_bytes(8, "big"),
+        arc200_app_id.to_bytes(8, "big"),
+    ]
 
     txn = transaction.ApplicationCreateTxn(
         sender=sender.getAddress(),
@@ -103,101 +99,197 @@ def create_ARC72_NFT(
     assert response.applicationIndex is not None and response.applicationIndex > 0
     return response.applicationIndex
 
-################
-################################################################################
-################
 
+def setup_ARC200_ARC72Sale_App(
 
-def ARC72_Transfer(client: AlgodClient, nft_app_id: int, sender:Account, receiver: Account, nft_id: int) -> None:
-    """Call arc72_transfer on the NFT contract, must be from the owner
+    client: AlgodClient,
+    appID: int,
+    funder: Account,
+) -> None:
+    """Finish setting up an Sale.
+
+    This operation funds the app Sale escrow account, opts that account into
+    the NFT, and sends the NFT to the escrow account, all in one atomic
+    transaction group. The Sale must not have started yet.
+
+    The escrow account requires a total of 0.203 Algos for funding. See the code
+    below for a breakdown of this amount.
 
     Args:
-        client: An Algod client.
-        appID: The app ID of the ARC72 NFT.
-        receiver: The account receiving the nft (becoming owner)
+        client: An algod client.
+        appID: The app ID of the Sale.
+        funder: The account providing the funding for the escrow account.
+        nftID: The NFT ID
     """
-    appAddr = get_application_address(nft_app_id)
-    appGlobalState = getAppGlobalState(client, nft_app_id)
-
-    app_args = [b"arc72_transferFrom"]
-    app_args.append(
-        encoding.decode_address(sender.getAddress())
-    )
-    if isinstance(receiver, str):
-        app_args.append(
-            encoding.decode_address(receiver)
-        )
-    else:
-        app_args.append(
-            encoding.decode_address(receiver.getAddress())
-        )
-    app_args.append(nft_id)
-    # app_args.append(receiver.getAddress())
+    appAddr = get_application_address(appID)
 
     suggestedParams = client.suggested_params()
 
-    accounts: List[str] = [encoding.encode_address(appGlobalState[b"owner"])]
-    # accounts.append(receiver.getAddress())
-
-    if isinstance(receiver, str):
-        receiver_str = receiver
-    else:
-        receiver_str = receiver.getAddress()
-
-    app_args.append(
-        receiver_str
+    fundingAmount = (
+        # min account balance
+        400_000+ 10_000
     )
-    print("ARC_72 call: app_args = ",[b"arc72_transferFrom", sender.getAddress(), receiver_str, nft_id," on ",nft_app_id])
 
-    appCallTxn = transaction.ApplicationCallTxn(
-        sender=sender.getAddress(),
-        index=nft_app_id,
+    fundAppTxn = transaction.PaymentTxn(
+        sender=funder.getAddress(),
+        receiver=appAddr,
+        amt=fundingAmount,
+        sp=suggestedParams,
+    )
+
+    transaction.assign_group_id([fundAppTxn])
+
+    signedFundAppTxn = fundAppTxn.sign(funder.getPrivateKey())
+    client.send_transactions([signedFundAppTxn])
+    waitForTransaction(client, signedFundAppTxn.get_txid())
+
+
+def updateSalePrice(
+    client: AlgodClient,
+    appID: int,
+    funder: Account,
+    price: int,
+) -> None:
+    """Finish setting up an Sale.
+
+    """
+    suggestedParams = client.suggested_params()
+
+    app_args =[b"update_price"]
+    app_args.append( (price.to_bytes(8, "big")) )
+
+
+    updateTxn = transaction.ApplicationCallTxn(
+        sender=funder.getAddress(),
+        index=appID,
         on_complete=transaction.OnComplete.NoOpOC,
         app_args=app_args,
+        sp=suggestedParams,
+    )
+
+    signedupdateTxn = updateTxn.sign(funder.getPrivateKey())
+
+    client.send_transactions([signedupdateTxn])
+
+    waitForTransaction(client, signedupdateTxn.get_txid())
+
+
+
+def Buy_ARC200_ARC72Sale(client: AlgodClient, appID: int, nft_app_id: int, arc200_app_id, buyer: Account, price: int) -> None:
+    """Place a bid on an active Sale.
+
+    Args:
+        client: An Algod client.
+        appID: The app ID of the Sale.
+        buyer: The account providing the bid.
+        price: The amount of the bid.
+    """
+    appAddr = get_application_address(appID)
+    print("Buying:\t-appAddr = ",appAddr," with ", price,"  ARC200")
+    appGlobalState = getAppGlobalState(client, appID)
+
+    nftID = appGlobalState[b"nft_id"]
+
+    suggestedParams = client.suggested_params()
+    # First transaction: Buyer approves the Sale contract to spend `price` amount of ARC-200 tokens
+    app_args_ = [b"arc200_approve", encoding.decode_address(appAddr), price.to_bytes(8, "big")]
+    print("Approve Token ARC200 tx: ",[b"arc200_approve", appAddr, price])
+    approve_txn = transaction.ApplicationCallTxn(
+        sender=buyer.getAddress(),
+        index=arc200_app_id,
+        on_complete=transaction.OnComplete.NoOpOC,
+        app_args=app_args_,
+        sp=suggestedParams,
+    )
+    
+    # signedPayTxn = approve_txn.sign(buyer.getPrivateKey())
+    # client.send_transaction(signedPayTxn)
+    # waitForTransaction(client, signedPayTxn.get_txid())
+    # print("Approve done.")
+
+    foreign_apps = [arc200_app_id,nft_app_id]
+    foreign_accounts: List[str] = [encoding.encode_address(appGlobalState[b"seller"])]    
+    foreign_accounts.append(buyer.getAddress())
+    # print("Foreign accounts = ",foreign_accounts)
+    # Second transaction: Buy call to the Sale contract
+    buy_txn = transaction.ApplicationCallTxn(
+        sender=buyer.getAddress(),
+        index=appID,
+        on_complete=transaction.OnComplete.NoOpOC,
+        app_args=[b"buy"],
+        accounts=foreign_accounts,
+        foreign_apps=foreign_apps,
+        sp=suggestedParams,
+    )
+    print("Buy on ARC200>ARC72 Sale App tx: ",[b"buy", appID, price])
+
+    transaction.assign_group_id([approve_txn, buy_txn])
+    signedPayTxn = approve_txn.sign(buyer.getPrivateKey())
+    signedAppCallTxn = buy_txn.sign(buyer.getPrivateKey())
+    client.send_transactions([signedPayTxn, signedAppCallTxn])
+    waitForTransaction(client, signedAppCallTxn.get_txid())
+
+
+def cancelSale(
+    client: AlgodClient,
+    appID: int
+) -> None:
+    """Finish setting up an Sale.
+
+    """
+    suggestedParams = client.suggested_params()
+
+    app_args =[b"cancel"]
+    accounts: List[str] = [encoding.encode_address(appGlobalState[b"seller"])]
+    foreign_apps = [appGlobalState[b"arc200_app_id"], appGlobalState[b"nft_app_id"]]
+
+    updateTxn = transaction.ApplicationCallTxn(
+        sender=funder.getAddress(),
+        index=appID,
+        on_complete=transaction.OnComplete.NoOpOC,
+        app_args=app_args,
+        foreign_apps=foreign_apps,
         accounts=accounts,
         sp=suggestedParams,
     )
-    print("Signing ApplicationCallTxn from Sender:",sender.getAddress())
-    signedAppCallTxn = appCallTxn.sign(sender.getPrivateKey())
-    print("broadcasting...")
-    client.send_transaction(signedAppCallTxn)
-    waitForTransaction(client, appCallTxn.get_txid())
-    
-################
-################################################################################
-################
 
-def close_ARC72_NFT(client: AlgodClient, appID: int, closer: Account, nftID):
-    """Close an Offer.
+    signedupdateTxn = updateTxn.sign(funder.getPrivateKey())
 
-    This action can only happen before an Offer has begun, in which case it is
-    cancelled, or after an Offer has ended.
+    client.send_transactions([signedupdateTxn])
 
-    If called after the Offer has ended and the Offer was successful, the
-    NFT is transferred to the winning bidder and the Offer proceeds are
-    transferred to the seller. If the Offer was not successful, the NFT and
+    waitForTransaction(client, signedupdateTxn.get_txid())
+
+
+
+def close_ARC200_ARC72Sale(client: AlgodClient, appID: int, closer: Account) -> None:
+    """Close an Sale.
+
+    This action can only happen before an Sale has begun, in which case it is
+    cancelled, or after an Sale has ended.
+
+    If called after the Sale has ended and the Sale was successful, the
+    NFT is transferred to the winning bidder and the Sale proceeds are
+    transferred to the seller. If the Sale was not successful, the NFT and
     all funds are transferred to the seller.
 
     Args:
         client: An Algod client.
-        appID: The app ID of the Offer.
+        appID: The app ID of the Sale.
         closer: The account initiating the close transaction. This must be
-            either the seller or Offer creator if you wish to close the
-            Offer before it starts. Otherwise, this can be any account.
+            either the seller or Sale creator if you wish to close the
+            Sale before it starts. Otherwise, this can be any account.
     """
     appGlobalState = getAppGlobalState(client, appID)
 
+    nftID = appGlobalState[b"nft_id"]
 
-    accounts: List[str] = [encoding.encode_address(appGlobalState[b"buyer"])]
-
-    if (b"seller" in appGlobalState):
-        accounts.append(encoding.encode_address(appGlobalState[b"seller"]))
-
+    accounts: List[str] = [encoding.encode_address(appGlobalState[b"seller"])]
+    foreign_apps = [appGlobalState[b"arc200_app_id"], appGlobalState[b"nft_app_id"]]
     deleteTxn = transaction.ApplicationDeleteTxn(
         sender=closer.getAddress(),
         index=appID,
         accounts=accounts,
-        foreign_assets=[nftID],
+        foreign_apps=foreign_apps,
         sp=client.suggested_params(),
     )
     signedDeleteTxn = deleteTxn.sign(closer.getPrivateKey())
