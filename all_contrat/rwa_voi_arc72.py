b5445b5b95b928f54933b0fb72edc6c18080734c
kilagg <audranthomas69@gmail.com>
2024-04-26 14:16:32 +0200

    push rwa

--- all_contrat/rwa_voi_arc72.py
+++ all_contrat/rwa_voi_arc72.py
@@ -1,31 +1,31 @@
 from pyteal import *
+from all_contrat.constants import FEES_ADDRESS, FEES
 
 
 def approval_program():
     # PARAMETERS
-    seller_key = Bytes("seller")
     price = Bytes("price")
     fees_address = Bytes("fees_address")
     name = Bytes("name")
     description = Bytes("description")
 
     @Subroutine(TealType.none)
-    def SendAlgoToSeller(amount: Expr) -> Expr:
+    def function_pay_seller() -> Expr:
         return Seq(
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields(
                 {
                     TxnField.type_enum: TxnType.Payment,
-                    TxnField.amount: amount,
+                    TxnField.amount: App.globalGet(price)-Int(FEES),
                     TxnField.sender: Global.current_application_address(),
-                    TxnField.receiver: App.globalGet(seller_key),
+                    TxnField.receiver: Global.creator_address(),
                 }
             ),
             InnerTxnBuilder.Submit(),
         )
 
     @Subroutine(TealType.none)
-    def SendNoteToFees(amount: Expr, note: Expr) -> Expr:
+    def function_send_note(amount: Expr, note: Expr) -> Expr:
         return Seq(
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields(
@@ -41,14 +41,14 @@ def approval_program():
         )
 
     @Subroutine(TealType.none)
-    def closeAccountTo(account: Expr) -> Expr:
+    def function_close_app() -> Expr:
         return If(Balance(Global.current_application_address()) != Int(0)).Then(
             Seq(
                 InnerTxnBuilder.Begin(),
                 InnerTxnBuilder.SetFields(
                     {
                         TxnField.type_enum: TxnType.Payment,
-                        TxnField.close_remainder_to: account,
+                        TxnField.close_remainder_to: Global.creator_address(),
                     }
                 ),
                 InnerTxnBuilder.Submit(),
@@ -56,73 +56,64 @@ def approval_program():
         )
 
     on_create = Seq(
-        App.globalPut(seller_key, Txn.application_args[0]),
-        App.globalPut(price, Btoi(Txn.application_args[1])),
-        App.globalPut(fees_address, Txn.application_args[2]),
-        App.globalPut(name, Txn.application_args[3]),
-        App.globalPut(description, Txn.application_args[4]),
+        App.globalPut(price, Btoi(Txn.application_args[0])),
+        App.globalPut(fees_address, Txn.application_args[1]),
+        App.globalPut(name, Txn.application_args[2]),
+        App.globalPut(description, Txn.application_args[3]),
         Approve(),
     )
 
-    on_buy_txn_index = Txn.group_index() - Int(1)
     on_buy = Seq(
         Assert(
             And(
-                Gtxn[on_buy_txn_index].amount() == App.globalGet(price),
-                Gtxn[on_buy_txn_index].receiver() == Global.current_application_address(),
-                Gtxn[on_buy_txn_index].type_enum() == TxnType.Payment,
-                Gtxn[on_buy_txn_index].sender() == Txn.sender(),
+                Gtxn[Txn.group_index() - Int(1)].amount() == App.globalGet(price),
+                Gtxn[Txn.group_index() - Int(1)].receiver() == Global.current_application_address(),
+                Gtxn[Txn.group_index() - Int(1)].type_enum() == TxnType.Payment,
+                Gtxn[Txn.group_index() - Int(1)].sender() == Txn.sender(),
             )
         ),
         Seq(
-            SendNoteToFees(Int(0), Bytes("sale,buy,1/rwa")),
-            SendAlgoToSeller(Gtxn[on_buy_txn_index].amount()-Int(0)),
+            function_send_note(Int(0), Bytes("sale,buy,1/rwa")),
+            function_pay_seller(),
+            function_close_app(),
             Approve()
         ),
         Reject(),
     )
 
-    new_price = Btoi(Txn.application_args[1])
     on_update_price = Seq(
         Assert(
             And(
-                Txn.sender() == App.globalGet(seller_key),
-                new_price > Int(0)
+                Txn.sender() == Global.creator_address(),
+                Btoi(Txn.application_args[1]) > Int(0)
             )
         ),
         Seq(
-            App.globalPut(price, new_price),
-            SendNoteToFees(Int(0), Bytes("sale,update,1/rwa")),
+            function_send_note(Int(0), Bytes("sale,update,1/rwa")),
+            App.globalPut(price, Btoi(Txn.application_args[1])),
             Approve()
         ),
         Reject()
     )
 
-    on_call_method = Txn.application_args[0]
     on_call = Cond(
-        [on_call_method == Bytes("buy"), on_buy],
-        [on_call_method == Bytes("update_price"), on_update_price]
+        [Txn.application_args[0] == Bytes("buy"), on_buy],
+        [Txn.application_args[0] == Bytes("update_price"), on_update_price]
     )
 
     on_delete = Seq(
         Assert(
-            Or(
-                Txn.sender() == App.globalGet(seller_key),
-                Txn.sender() == Global.creator_address()
-            )
+            Txn.sender() == Global.creator_address()
         ),
-        SendNoteToFees(Int(0), Bytes("sale,close,1/rwa")),
-        closeAccountTo(App.globalGet(seller_key)),
+        function_send_note(Int(0), Bytes("sale,close,1/rwa")),
+        function_close_app(),
         Approve(),
     )
 
     program = Cond(
         [Txn.application_id() == Int(0), on_create],
         [Txn.on_completion() == OnComplete.NoOp, on_call],
-        [
-            Txn.on_completion() == OnComplete.DeleteApplication,
-            on_delete,
-        ],
+        [Txn.on_completion() == OnComplete.DeleteApplication, on_delete],
         [
             Or(
                 Txn.on_completion() == OnComplete.OptIn,
