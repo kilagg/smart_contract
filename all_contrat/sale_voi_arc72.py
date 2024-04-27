e3099f10d0981df38fb2e12898fbfef7cc4707f8
kilagg <audranthomas69@gmail.com>
2024-04-27 16:54:58 +0200

    last version for sale_voi_arc72.py

--- all_contrat/sale_voi_arc72.py
+++ all_contrat/sale_voi_arc72.py
@@ -1,40 +1,40 @@
 from pyteal import *
-from all_contrat.constants import FEES_ADDRESS, FEES
+from all_contrat.constants import FEES_ADDRESS, ZERO_FEES, PURCHASE_FEES, CREATE_FEES
 
 
 def approval_program():
     # PARAMETERS
-    nft_id_key = Bytes("nft_id")
-    nft_app_id_key = Bytes("nft_app_id")
-    nft_price = Bytes("price")
+    nft_id = Bytes("nft_id")
+    nft_app_id = Bytes("nft_app_id")
+    price = Bytes("price")
     fees_address = Bytes("fees_address")
 
     @Subroutine(TealType.none)
-    def function_transfert_nft(to_account: Expr) -> Expr:
+    def function_transfer_arc72(to: Expr) -> Expr:
         return Seq(
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields({
                 TxnField.type_enum: TxnType.ApplicationCall,
-                TxnField.application_id: App.globalGet(nft_app_id_key),
+                TxnField.application_id: App.globalGet(nft_app_id),
                 TxnField.on_completion: OnComplete.NoOp,
                 TxnField.application_args: [
                     Bytes("base16", "f2f194a0"),
                     Global.creator_address(),
-                    to_account,
-                    App.globalGet(nft_id_key)
+                    to,
+                    App.globalGet(nft_id)
                 ],
             }),
             InnerTxnBuilder.Submit(),
         )
 
     @Subroutine(TealType.none)
-    def function_pay_seller() -> Expr:
+    def function_payment() -> Expr:
         return Seq(
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields(
                 {
                     TxnField.type_enum: TxnType.Payment,
-                    TxnField.amount: App.globalGet(nft_price)-Int(FEES),
+                    TxnField.amount: App.globalGet(price)-Int(PURCHASE_FEES),
                     TxnField.sender: Global.current_application_address(),
                     TxnField.receiver: Global.creator_address(),
                 }
@@ -74,33 +74,33 @@ def approval_program():
         )
 
     on_create = Seq(
-        App.globalPut(nft_app_id_key, Btoi(Txn.application_args[0])),
-        App.globalPut(nft_id_key, Txn.application_args[1]),
-        App.globalPut(nft_price, Btoi(Txn.application_args[2])),
-        App.globalPut(fees_address, Txn.application_args[3]),
+        App.globalPut(nft_app_id, Btoi(Txn.application_args[0])),
+        App.globalPut(nft_id, Txn.application_args[1]),
+        App.globalPut(price, Btoi(Txn.application_args[2])),
+        App.globalPut(fees_address, Addr(FEES_ADDRESS)),
         Approve(),
     )
 
     on_buy = Seq(
         Assert(
             And(
-                Gtxn[Txn.group_index() - Int(1)].amount() == App.globalGet(nft_price),
+                Gtxn[Txn.group_index() - Int(1)].amount() == App.globalGet(price),
                 Gtxn[Txn.group_index() - Int(1)].receiver() == Global.current_application_address(),
                 Gtxn[Txn.group_index() - Int(1)].type_enum() == TxnType.Payment,
                 Gtxn[Txn.group_index() - Int(1)].sender() == Txn.sender(),
             )
         ),
         Seq(
-            function_send_note(Int(0), Bytes("sale,buy,1/72")),
-            function_pay_seller(),
-            function_transfert_nft(Gtxn[Txn.group_index() - Int(1)].sender()),
+            function_payment(),
+            function_transfer_arc72(Gtxn[Txn.group_index() - Int(1)].sender()),
+            function_send_note(Int(PURCHASE_FEES), Bytes("sale,buy,1/72")),
             function_close_app(),
             Approve()
         ),
         Reject(),
     )
 
-    on_update_price = Seq(
+    on_update = Seq(
         Assert(
             And(
                 Txn.sender() == Global.creator_address(),
@@ -108,32 +108,28 @@ def approval_program():
             )
         ),
         Seq(
-            function_send_note(Int(0), Bytes("sale,update,1/72")),
-            App.globalPut(nft_price, Btoi(Txn.application_args[1])),
+            App.globalPut(price, Btoi(Txn.application_args[1])),
+            function_send_note(Int(ZERO_FEES), Bytes("sale,update,1/72")),
             Approve()
         ),
         Reject()
-    )
-
-    on_call = Cond(
-        [Txn.application_args[0] == Bytes("pre_validate"), Approve()],
-        [Txn.application_args[0] == Bytes("buy"), on_buy],
-        [Txn.application_args[0] == Bytes("update_price"), on_update_price]
     )
 
     on_delete = Seq(
         Assert(
             Txn.sender() == Global.creator_address()
         ),
-        function_send_note(Int(0), Bytes("sale,close,1/72")),
+        function_send_note(Int(ZERO_FEES), Bytes("sale,close,1/72")),
         function_close_app(),
         Approve(),
     )
 
     program = Cond(
         [Txn.application_id() == Int(0), on_create],
-        [Txn.on_completion() == OnComplete.NoOp, on_call],
         [Txn.on_completion() == OnComplete.DeleteApplication, on_delete],
+        [And(Txn.on_completion() == OnComplete.NoOp, Txn.application_args[0] == Bytes("update_price")), on_update],
+        [And(Txn.on_completion() == OnComplete.NoOp, Txn.application_args[0] == Bytes("pre_validate")), Approve()],
+        [And(Txn.on_completion() == OnComplete.NoOp, Txn.application_args[0] == Bytes("buy")), on_buy],
         [
             Or(
                 Txn.on_completion() == OnComplete.OptIn,
