b803e033cd83909cb54bf97be97bd00448a84172
kilagg <audranthomas69@gmail.com>
2024-04-28 10:51:43 +0200

    factozisation dutch

--- all_contrat/dutch_voi_arc72.py
+++ all_contrat/dutch_voi_arc72.py
@@ -1,80 +1,11 @@
 from pyteal import *
-from all_contrat.constants import FEES_ADDRESS, ZERO_FEES, PURCHASE_FEES, CREATE_FEES
+from all_contrat.constants import FEES_ADDRESS, PURCHASE_FEES
+from all_contrat.subroutine import nft_id, nft_app_id, fees_address
+from all_contrat.subroutine import function_send_note, function_close_app, function_transfer_arc72, function_payment
+from all_contrat.subroutine import on_delete, on_fund, nft_max_price, nft_min_price, start_time_key, end_time_key
 
 
 def approval_program():
-    # PARAMETERS
-    nft_id = Bytes("nft_id")
-    nft_app_id = Bytes("nft_app_id")
-    nft_max_price = Bytes("max_price")
-    nft_min_price = Bytes("min_price")
-    fees_address = Bytes("fees_address")
-    start_time_key = Bytes("start")
-    end_time_key = Bytes("end")
-
-    @Subroutine(TealType.none)
-    def function_transfer_arc72(to: Expr) -> Expr:
-        return Seq(
-            InnerTxnBuilder.Begin(),
-            InnerTxnBuilder.SetFields({
-                TxnField.type_enum: TxnType.ApplicationCall,
-                TxnField.application_id: App.globalGet(nft_app_id),
-                TxnField.on_completion: OnComplete.NoOp,
-                TxnField.application_args: [
-                    Bytes("base16", "f2f194a0"),
-                    Global.creator_address(),
-                    to,
-                    App.globalGet(nft_id)
-                ],
-            }),
-            InnerTxnBuilder.Submit(),
-        )
-
-    @Subroutine(TealType.none)
-    def function_payment(amount: Expr) -> Expr:
-        return Seq(
-            InnerTxnBuilder.Begin(),
-            InnerTxnBuilder.SetFields(
-                {
-                    TxnField.type_enum: TxnType.Payment,
-                    TxnField.amount: amount-Int(PURCHASE_FEES),
-                    TxnField.sender: Global.current_application_address(),
-                    TxnField.receiver: Global.creator_address(),
-                }
-            ),
-            InnerTxnBuilder.Submit(),
-        )
-
-    @Subroutine(TealType.none)
-    def function_send_note(amount: Expr, note: Expr) -> Expr:
-        return Seq(
-            InnerTxnBuilder.Begin(),
-            InnerTxnBuilder.SetFields(
-                {
-                    TxnField.type_enum: TxnType.Payment,
-                    TxnField.amount: amount,
-                    TxnField.sender: Global.current_application_address(),
-                    TxnField.receiver: App.globalGet(fees_address),
-                    TxnField.note: note,
-                }
-            ),
-            InnerTxnBuilder.Submit(),
-        )
-
-    @Subroutine(TealType.none)
-    def function_close_app() -> Expr:
-        return If(Balance(Global.current_application_address()) != Int(0)).Then(
-            Seq(
-                InnerTxnBuilder.Begin(),
-                InnerTxnBuilder.SetFields(
-                    {
-                        TxnField.type_enum: TxnType.Payment,
-                        TxnField.close_remainder_to: Global.creator_address(),
-                    }
-                ),
-                InnerTxnBuilder.Submit(),
-            )
-        )
 
     on_create = Seq(
         App.globalPut(nft_app_id, Btoi(Txn.application_args[0])),
@@ -120,7 +51,7 @@ def approval_program():
             )
         ),
         Seq(
-            function_payment(Gtxn[Txn.group_index() - Int(1)].amount()),
+            function_payment(Gtxn[Txn.group_index() - Int(1)].amount()-Int(PURCHASE_FEES)),
             function_transfer_arc72(Txn.sender()),
             function_send_note(Int(PURCHASE_FEES), Bytes("dutch,buy,1/72")),
             function_close_app(),
@@ -129,16 +60,10 @@ def approval_program():
         Reject(),
     )
 
-    on_delete = Seq(
-        Assert(Txn.sender() == Global.creator_address()),
-        function_send_note(Int(ZERO_FEES), Bytes("dutch,close,1/72")),
-        function_close_app(),
-        Approve()
-    )
-
     program = Cond(
         [Txn.application_id() == Int(0), on_create],
-        [Txn.on_completion() == OnComplete.DeleteApplication, on_delete],
+        [Txn.on_completion() == OnComplete.DeleteApplication, on_delete("dutch,close,1/72")],
+        [And(Txn.on_completion() == OnComplete.NoOp, Txn.application_args[0] == Bytes("fund")), on_fund("dutch,fund,1/72")],
         [And(Txn.on_completion() == OnComplete.NoOp, Txn.application_args[0] == Bytes("pre_validate")), Approve()],
         [And(Txn.on_completion() == OnComplete.NoOp, Txn.application_args[0] == Bytes("buy")), on_buy],
         [
@@ -152,17 +77,3 @@ def approval_program():
     )
 
     return program
-
-
-if __name__ == "__main__":
-    compiled = compileTeal(approval_program(), mode=Mode.Application, version=10)
-    from algosdk.v2client.algod import AlgodClient
-
-    algod_token_tx = ""
-    headers_tx = {"X-Algo-API-Token": algod_token_tx}
-    client = AlgodClient(
-        algod_token=algod_token_tx,
-        algod_address="https://testnet-api.voi.nodly.io:443",
-        headers=headers_tx,
-    )
-    print(client.compile(compiled)['result'])
